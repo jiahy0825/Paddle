3a7e990086397d0e0c305e77e93b3af54c8d1185
tianchao20042008 <124029667+tianchao20042008@users.noreply.github.com>
2023-10-30 17:55:57 +0800

    Adt dynamic shape (#72)
    * api for SymbolicDimExpr
    
    * symbolic_dim_expr_match_trait.h
    
    * add passes FoldConstantUnit, RemoveRedundantConstantRhs*
    
    ---------
    
    Co-authored-by: Tian Chao <>

--- paddle/cinn/adt/symbolic_dim_expr_simplifier.cc
+++ paddle/cinn/adt/symbolic_dim_expr_simplifier.cc
@@ -82,6 +82,21 @@ struct SwapConstantLhs {
   }
 };
 
+template <template<typename, typename> class Op, int unit>
+struct FoldConstantUnit {
+  using source_pattern_type = Op<SymbolicDimExpr, std::int64_t>;
+
+  SymbolicDimExpr MatchAndRewrite(SymbolicDimExpr&& expr) {
+    const auto& [lhs, rhs] = expr.Get<Op<SymbolicDimExpr, SymbolicDimExpr>>().tuple();
+    if (rhs.Get<std::int64_t>() == unit) {
+      return lhs;
+    } else {
+      return expr;
+    }
+    LOG(FATAL) << "Dead code.";
+  }
+};
+
 struct RemoveRedundantConstantLhs_Add_Add {
   using source_pattern_type = Add<Add<SymbolicDimExpr, std::int64_t>, std::int64_t>;
 
@@ -152,6 +167,80 @@ struct RemoveRedundantConstantLhs_Sub_Add {
   }
 };
 
+struct RemoveRedundantConstantLhs_Mul_Mul {
+  using source_pattern_type = Mul<Mul<SymbolicDimExpr, std::int64_t>, std::int64_t>;
+
+  SymbolicDimExpr MatchAndRewrite(SymbolicDimExpr&& expr) {
+    auto& [outter_lhs, outter_rhs] = *expr.Mut<Mul<SymbolicDimExpr, SymbolicDimExpr>>()->mut_tuple();
+    auto& [inner_lhs, inner_rhs] = *outter_lhs.Mut<Mul<SymbolicDimExpr, SymbolicDimExpr>>()->mut_tuple();
+    inner_lhs = Simplify(std::Move(inner_lhs));
+    inner_rhs = SymbolicDimExpr(outter_rhs.Get<std::int64_t>() * inner_rhs.Get<std::int64_t>());
+    return outter_lhs;
+  }
+};
+
+struct RemoveRedundantConstantLhs_Mul_Div {
+  using source_pattern_type = Mul<Div<SymbolicDimExpr, std::int64_t>, std::int64_t>;
+
+  SymbolicDimExpr MatchAndRewrite(SymbolicDimExpr&& expr) {
+    auto& [outter_lhs, outter_rhs] = *expr.Mut<Mul<SymbolicDimExpr, SymbolicDimExpr>>()->mut_tuple();
+    auto& [inner_lhs, inner_rhs] = *outter_lhs.Mut<Div<SymbolicDimExpr, SymbolicDimExpr>>()->mut_tuple();
+    if (outter_rhs.Get<std::int64_t>() == inner_rhs.Get<std::int64_t>()) {
+      return Simplify(std::Move(inner_lhs));
+    } else if (outter_rhs.Get<std::int64_t>() % inner_rhs.Get<std::int64_t>() == 0) {
+      std::int64_t ret_rhs = outter_rhs.Get<std::int64_t>() / inner_rhs.Get<std::int64_t>();
+      outter_lhs = Simplify(std::Move(inner_lhs));
+      outter_rhs = ret_rhs;
+      return expr;
+    } else if (inner_rhs.Get<std::int64_t>() % outter_rhs.Get<std::int64_t>() == 0) {
+      std::int64_t ret_rhs = inner_rhs.Get<std::int64_t>() / outter_rhs.Get<std::int64_t>();
+      inner_lhs = Simplify(std::Move(inner_lhs));
+      inner_rhs = SimplicDimExpr{ret_rhs};
+      return outter_lhs;
+    } else {
+      return expr;
+    }
+    LOG(FATAL) << "Dead code.";
+  }
+};
+
+struct RemoveRedundantConstantLhs_Div_Div {
+  using source_pattern_type = Div<Div<SymbolicDimExpr, std::int64_t>, std::int64_t>;
+
+  SymbolicDimExpr MatchAndRewrite(SymbolicDimExpr&& expr) {
+    auto& [outter_lhs, outter_rhs] = *expr.Mut<Div<SymbolicDimExpr, SymbolicDimExpr>>()->mut_tuple();
+    auto& [inner_lhs, inner_rhs] = *outter_lhs.Mut<Div<SymbolicDimExpr, SymbolicDimExpr>>()->mut_tuple();
+    inner_lhs = Simplify(std::Move(inner_lhs));
+    inner_rhs = SymbolicDimExpr(outter_rhs.Get<std::int64_t>() * inner_rhs.Get<std::int64_t>());
+    return outter_lhs;
+  }
+};
+
+struct RemoveRedundantConstantLhs_Div_Mul {
+  using source_pattern_type = Div<Mul<SymbolicDimExpr, std::int64_t>, std::int64_t>;
+
+  SymbolicDimExpr MatchAndRewrite(SymbolicDimExpr&& expr) {
+    auto& [outter_lhs, outter_rhs] = *expr.Mut<Div<SymbolicDimExpr, SymbolicDimExpr>>()->mut_tuple();
+    auto& [inner_lhs, inner_rhs] = *outter_lhs.Mut<Mul<SymbolicDimExpr, SymbolicDimExpr>>()->mut_tuple();
+    if (outter_rhs.Get<std::int64_t>() == inner_rhs.Get<std::int64_t>()) {
+      return Simplify(std::Move(inner_lhs));
+    } else if (outter_rhs.Get<std::int64_t>() % inner_rhs.Get<std::int64_t>() == 0) {
+      std::int64_t ret_rhs = outter_rhs.Get<std::int64_t>() / inner_rhs.Get<std::int64_t>();
+      outter_lhs = Simplify(std::Move(inner_lhs));
+      outter_rhs = ret_rhs;
+      return expr;
+    } else if (inner_rhs.Get<std::int64_t>() % outter_rhs.Get<std::int64_t>() == 0) {
+      std::int64_t ret_rhs = inner_rhs.Get<std::int64_t>() / outter_rhs.Get<std::int64_t>();
+      inner_lhs = Simplify(std::Move(inner_lhs));
+      inner_rhs = SimplicDimExpr{ret_rhs};
+      return outter_lhs;
+    } else {
+      return expr;
+    }
+    LOG(FATAL) << "Dead code.";
+  }
+};
+
 SymbolicDimExpr Simplify(SymbolicDimExpr&& expr) {
   expr = TrySimplifyPass<FoldConstantAdd>(std::move(expr));
   expr = TrySimplifyPass<FoldConstantSub>(std::move(expr));
@@ -163,13 +252,21 @@ SymbolicDimExpr Simplify(SymbolicDimExpr&& expr) {
   expr = TrySimplifyPass<SwapConstantLhs<Mul>>(std::move(expr));
   expr = TrySimplifyPass<SwapConstantLhs<Div>>(std::move(expr));
   expr = TrySimplifyPass<SwapConstantLhs<BroadcastedDim>>(std::move(expr));
+  expr = TrySimplifyPass<FoldConstantUnit<Add, 0>>(std::move(expr));
+  expr = TrySimplifyPass<FoldConstantUnit<Sub, 0>>(std::move(expr));
+  expr = TrySimplifyPass<FoldConstantUnit<Mul, 1>>(std::move(expr));
+  expr = TrySimplifyPass<FoldConstantUnit<Div, 1>>(std::move(expr));
+  expr = TrySimplifyPass<FoldConstantUnit<BroadcastedDim, 1>>(std::move(expr));
   expr = TrySimplifyPass<RemoveRedundantConstantLhs_Add_Add>(std::move(expr));
   expr = TrySimplifyPass<RemoveRedundantConstantLhs_Add_Sub>(std::move(expr));
   expr = TrySimplifyPass<RemoveRedundantConstantLhs_Sub_Add>(std::move(expr));
   expr = TrySimplifyPass<RemoveRedundantConstantLhs_Sub_Sub>(std::move(expr));
+  expr = TrySimplifyPass<RemoveRedundantConstantLhs_Mul_Mul>(std::move(expr));
+  expr = TrySimplifyPass<RemoveRedundantConstantLhs_Mul_Div>(std::move(expr));
+  expr = TrySimplifyPass<RemoveRedundantConstantLhs_Div_Mul>(std::move(expr));
+  expr = TrySimplifyPass<RemoveRedundantConstantLhs_Div_Div>(std::move(expr));
   return expr;
 }
-
 
 }  // namespace
 
